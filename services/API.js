456721f0c1b5bc7bf932d7437cf886ada924fad6
unknown <bilimdepaullilian@gmail.com>
2025-06-12 23:10:06 +0100

    some changes

--- services/API.js
+++ services/API.js
@@ -1,10 +1,23 @@
 const API = {
   url: "https://dummyjson.com/products",
   fetchMenu: async () => {
+    try {
       const result = await fetch(API.url);
-    return  await result.json()
+      //we store the products in the localstorage
+      localStorage.setItem("products", JSON.stringify(await result.json()));
+      return await result.json();
+    } catch (error) {
+      //we  check the localstorage to see if there are products store there
+      const storedProducts = localStorage.getItem("products");
+      if (storedProducts) {
+        return JSON.parse(storedProducts);
+      }
+      console.error("Error fetching menu:", error);
+      return null;
+    } finally {
+      console.log("fetching menu completed");
+    }
   },
 };
 
-
-export default API
+export default API;
